233b81285d4df2306a3b4cd58f65a2963ffeaf94
programmer-er <jhariharan2004@gmail.com>
2023-10-06 12:50:31 +0530

    Added loading and error states for /login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,13 +3,22 @@
 import { useState } from 'react'
 import s from './login.module.css'
 import ENDPOINT from '@/helpers/endpoint'
+import { useRouter } from 'next/router'
 
 export default function Login() {
 	let [email, setEmail] = useState('')
 	let [password, setPassword] = useState('')
 
+	let [loading, setLoading] = useState(false)
+	let [error, setError] = useState(false)
+
+	let router = useRouter()
+
 	async function submit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault()
+
+		setLoading(true)
+		setError(false)
 
 		let res = await fetch(ENDPOINT + '/login', {
 			method: 'POST',
@@ -17,11 +26,14 @@ export default function Login() {
 		})
 		
 		if (res.status == 400) {
-			alert("wrong credentials")
+			setError(true)
+			setLoading(false)
 		} else {
 			let data = await res.json()
 			document.cookie = `user=${data.jwt}`
+			router.push("/questions")
 		}
+
 
 		setEmail('')
 		setPassword('')
@@ -53,6 +65,12 @@ export default function Login() {
 				<br />
 
 				<button type="submit">Submit</button>
+				
+				<br />
+
+				{ loading ? <p>loading</p> : null }
+				{ error ? <p>wrong credentials</p> : null }
+
 			</form>
 		</main>
 	)
