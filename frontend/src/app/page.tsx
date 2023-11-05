72c7e396f895df752830a0218f3f315efdd023c4
Testing wala User <apoorva3569@gmail.com>
2023-11-05 22:21:40 +0530

    Home Page Styling and Backend issues fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,28 @@
 'use client'
 import Cookies from 'js-cookie';
-
-import Link from 'next/link'
-import s from './home.module.css'
+import Link from 'next/link';
+import s from './home.module.css';
 
 export default function Home() {
-	const username = Cookies.get('username');
-	console.log('Username from cookie:', username);
-	return (
-		<main className = {`${s.home} pd-top`}>
-			<div className = {s.content}>
-				<h1>IEEE Eureka</h1>
-				<p>Get ready for action!</p><br />
-				<h2>
-				{username && ( <span className={s.welcome_team}>!!Welcome to the EUREKA CTF TEAM {username}!!</span> )}
-				</h2><br />
-				<div className = {s.buttons}>
-					<Link href = "/login">Login</Link>
-					<Link href = "/register">Register</Link>
-					<Link href = "/logout">Logout</Link>
-				</div>
-			</div>
-		</main>
-	)
+  const username = Cookies.get('username');
+  console.log('Username from cookie:', username);
+
+  return (
+    <main className={`${s.home} pd-top`}>
+      <div className={s.content}>
+        <h1 className={s.title}>IEEE Eureka CTF 2023</h1>
+        <p className={s.subtitle}>Get ready for action!</p>
+        {username && (
+          <h2 className={s.welcomeTeam}>
+            Welcome to the EUREKA CTF TEAM <span>{username}</span>!
+          </h2>
+        )}
+        <div className={s.buttons}>
+			<Link href="/login" className={s.button}>Login</Link>
+			<Link href="/register" className={s.button}>Register</Link>
+            <Link href="/logout" className={s.button}>Logout</Link>
+        </div>
+      </div>
+    </main>
+  );
 }
