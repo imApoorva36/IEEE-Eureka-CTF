826b16a7d2116e272943b7441dfc98f665264e63
Vignaraj-pai <vignarajpai@outlook.com>
2023-11-09 03:00:24 +0530

    Added some login styles + ParticleJs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,8 +5,13 @@ import s from './home.module.css';
 import { Lexend } from 'next/font/google'
 import { League_Spartan } from 'next/font/google'
 
+import Design from '@/components/Design';
+
 const lexend = Lexend({ subsets: ['latin'] })
 const league_spartan = League_Spartan({ subsets: ['latin'] })
+
+
+
 
 export default function Home() {
   const username = Cookies.get('username');
@@ -14,6 +19,7 @@ export default function Home() {
 
   return (
     <main className={s.home + ' ' + lexend.className}>
+      <Design />
       <div className={s.content}>
         <h1 className={s.title}>IEEE Eureka CTF 2023</h1>
         <p className={s.subtitle}>Get ready for action!</p>
@@ -22,11 +28,20 @@ export default function Home() {
             Welcome to the EUREKA CTF TEAM <span>{username}</span>!
           </h2>
         )}
+
+        {!username && (
         <div className={s.buttons}>
             <Link href="/login" className={s.button}>Login</Link>
             <Link href="/register" className={s.button}>Register</Link>
             <Link href="/logout" className={s.button}>Logout</Link>
         </div>
+        )}
+
+      {username && (
+      <div className={s.logoutButton}>
+        <Link href="/logout" className={s.button}>Logout</Link>
+      </div>
+      )}
       </div>
     </main>
   );
---
+++ frontend/src/components/Design.tsx
@@ -0,0 +1,97 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+
+const Design = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+    return (<div> <Particles
+    id="tsparticles"
+    init={particlesInit}
+    loaded={particlesLoaded}
+    options={{
+        // background: {
+        //     color: {
+        //         value: "#0d47a1",
+        //     },
+        // },
+        fpsLimit: 60,
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: true,
+                    mode: "push",
+                },
+                onHover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                resize: true,
+            },
+            modes: {
+                push: {
+                    quantity: 2,
+                },
+                repulse: {
+                    distance: 150,
+                    duration: 0.4,
+                },
+            },
+        },
+        particles: {
+            color: {
+                // Transparent color
+                value: "rgb(255, 255, 255, 0.2)",
+            },
+            links: {
+                color: "rgb(255, 255, 255, 0.2)",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+            },
+            move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                    default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
+            },
+            number: {
+                density: {
+                    enable: true,
+                    area: 1200,
+                },
+                value: 80,
+            },
+            opacity: {
+                value: 0.2,
+            },
+            shape: {
+                type: "circle",
+            },
+            size: {
+                value: { min: 1, max: 2 },
+            },
+        },
+        detectRetina: true,
+    }}
+/> </div>)
+};
+
+export default Design;
