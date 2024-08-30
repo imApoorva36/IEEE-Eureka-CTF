2884e5542957f042f69fd9a2554386e451722801
vignarajpai@outlook.com <vignarajpai@outlook.com>
2024-08-30 17:12:06 +0530

    backend config

--- frontend/src/helpers/endpoint.ts
+++ frontend/src/helpers/endpoint.ts
@@ -1,3 +1,3 @@
-// const ENDPOINT = "https://ctf-backend.azurewebsites.net/api"
+// const ENDPOINT = "http://codered-ieee.nitk.ac.in/api"
 const ENDPOINT = "http://localhost:8000/api"
 export default ENDPOINT
