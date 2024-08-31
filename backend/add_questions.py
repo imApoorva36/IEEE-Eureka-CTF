ca4f3b9f5183fd8ada72ebaefbbdd0eef550b235
vignarajpai@outlook.com <vignarajpai@outlook.com>
2024-08-31 18:07:10 +0530

    questions

--- backend/add_questions.py
+++ backend/add_questions.py
@@ -13,20 +13,21 @@ def csv_to_dict(file_name, encoding='utf-8'):
         csv_reader = csv.DictReader(csv_file)
         return [row for row in csv_reader]
     
-def create_question(title, text, points, link, flag):
-    new_question = Question(title=title, text=text, points=points, link=link, flag=flag)
+def create_question(title, text, points, link, flag, section):
+    new_question = Question(title=title, text=text, points=points, link=link, flag=flag, section=section)
     new_question.save()
     return new_question
 
 def create_questions_from_csv(file_name):
     for row in csv_to_dict(file_name, encoding='utf-8'):
         title = row['title']
-        text = row['text']
+        text = row['description']
         points = row['points']
         link = row['link']
         flag = row['flag']
-        create_question(title, text, points, link, flag)
+        section = row['section']
+        create_question(title, text, points, link, flag, section)
         print(f'Created question {title} with flag {flag}')
 
 if __name__ == '__main__':
-    create_questions_from_csv('questions.csv')
+    create_questions_from_csv('questions_.csv')
