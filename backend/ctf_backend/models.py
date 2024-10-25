7730a874df75e507e9518ad7185d4e6289d8879a
Nishant A S <nishantas4043@gmail.com>
2024-10-25 23:37:33 +0530

    upgrade dep and add score field to team

--- backend/ctf_backend/models.py
+++ backend/ctf_backend/models.py
@@ -9,12 +9,13 @@ class Team(models.Model):
     member2 = models.CharField(max_length=30, null=True, blank=True)
     member3 = models.CharField(max_length=30, null=True, blank=True)
     contact = models.CharField(max_length=10, null=True, blank=True)
+    score = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.id} - {self.user} - {self.name}"
     
     def calculate_score(self):
-        responses = Flagresponse.objects.filter(team=self)
+        responses = FlagResponse.objects.filter(team=self)
         score = sum(response.question.points for response in responses)
         return score
 
@@ -24,17 +25,24 @@ class Question(models.Model):
     points = models.IntegerField()
     link = models.TextField(null=True, blank=True)
     flag = models.TextField(default="flag{N4LL_US34}")
+    
     def __str__(self):
         return f"{self.id} - {self.title} - {self.text} - {self.points} - {self.link} - {self.flag}"
     def user_response_count(self):
-        return Question.objects.filter(flagresponse__question=self).count()
+        return FlagResponse.objects.filter(question=self).count()
     def answered(self, user):
-        return Flagresponse.objects.filter(question=self, team__user=user).exists()
+        return FlagResponse.objects.filter(question=self, team__user=user).exists()
     
-class Flagresponse(models.Model):
+class FlagResponse(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     response = models.CharField(default='', max_length=50, null=True, blank=True)
+    
     def __str__(self):
         return f"{self.team} - {self.timestamp} - {self.question} - {self.response}"
+    
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['team', 'question'], name='unique_response')]
+        
+
--- backend/ctf_backend/serialisers.py
+++ backend/ctf_backend/serialisers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import Team, Question, Flagresponse
+from .models import Team, Question, FlagResponse
 from django.contrib.auth import get_user_model
 from rest_framework import status
 class TeamsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ('name','member1','member2','member3',)
+        fields = ('name','member1','member2','member3','score')
 
 class QuestionsSerializer(serializers.ModelSerializer):
     is_answered = serializers.SerializerMethodField()
@@ -16,16 +16,16 @@ class QuestionsSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         return obj.answered(user)
 
-class FlagresponsesSerializer(serializers.ModelSerializer):
+class FlagResponsesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Flagresponse
+        model = FlagResponse
         fields = ('team', 'timestamp', 'question', 'response')
     def create(self, validated_data):
         user = self.context['request'].user  # Get the current user
         response = validated_data.get('response')
         question = validated_data.get('question')
         timestamp = validated_data.get('timestamp')
-        flag_response = Flagresponse.objects.create(team=user.team, timestamp=timestamp, question=question, response=response)
+        flag_response = FlagResponse.objects.create(team=user.team, timestamp=timestamp, question=question, response=response)
         return flag_response
 
 class ScoreboardSerializer(serializers.Serializer):
