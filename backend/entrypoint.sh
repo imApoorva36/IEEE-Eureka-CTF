1764e845f14832067a550b55ce3861d7dc131fac
Akshat Chaturvedi <akshat.chaturvedi2003@gmail.com>
2024-11-02 04:13:03 +0530

    update deploy config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # python3 manage.py flush --no-input
+python3 manage.py makemigrations
 python3 manage.py migrate
 
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8080
+# python3 manage.py runserver 0.0.0.0:8000
 exec "$@"
