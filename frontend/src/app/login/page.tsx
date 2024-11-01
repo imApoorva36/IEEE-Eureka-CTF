c8e5a1147c0e1b79f2f2144df278abca15c2c1e1
sahil-mengji <mengjisahil@gmail.com>
2024-11-02 04:07:01 +0530

    added error messages and configured homepage bg

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,92 +1,101 @@
-'use client'
-import { useState } from 'react'
-import s from './login.module.css'
-import ENDPOINT from '@/helpers/endpoint'
-import { useRouter } from 'next/navigation';
-import { useCookies } from 'react-cookie';
-import Design from '@/components/Design';
+"use client";
+import { useState } from "react";
+import s from "./login.module.css";
+import ENDPOINT from "@/helpers/endpoint";
+import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
+import Design from "@/components/Design";
 
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter(); // Initialize the router object
-    const [cookies, setCookie] = useCookies(['access_token', 'refresh_token', 'username']);
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter(); // Initialize the router object
+	const [cookies, setCookie] = useCookies([
+		"access_token",
+		"refresh_token",
+		"username",
+	]);
 
-	let [loading, setLoading] = useState(false)
-	let [error, setError] = useState(false)
+	let [loading, setLoading] = useState(false);
+	let [error, setError] = useState(false);
 
-    async function submit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+	async function submit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
 
-        try {
-            const res = await fetch(ENDPOINT + '/token/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: email,  // Use email as the username
-                    password,
-                }),
+		try {
+			const res = await fetch(ENDPOINT + "/token/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					username: email, // Use email as the username
+					password,
+				}),
+			});
+			console.log(res);
+			if (res.status === 401) {
+				console.log("Wrong credentials");
+			} else if (res.ok) {
+				const data = await res.json();
+				setCookie("access_token", data.access, { path: "/" });
+				setCookie("refresh_token", data.refresh, { path: "/" });
+				setCookie("username", email, { path: "/" });
+				const username = email;
+				setUsername(email);
+				router.push("/questions");
+				console.log("Login Successfull!");
+			}
+		} catch (error) {
+			console.error("Login error:", error);
+			window.alert("Something went wrong, please try Logging in Again!!");
+		}
 
-            });
-            console.log(res);
-            if (res.status === 401) {
-                console.log('Wrong credentials');
-            } else if (res.ok) {
-                const data = await res.json();
-                setCookie('access_token', data.access, { path: '/' });
-                setCookie('refresh_token', data.refresh, { path: '/' });
-                setCookie('username', email, { path: '/' });
-                const username=email;
-                setUsername(email);
-                router.push('/questions');
-                console.log('Login Successfull!')
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-        }
+		setEmail("");
+		setPassword("");
+	}
 
-        setEmail('');
-        setPassword('');
-    }
+	return (
+		<main className={s.login}>
+			<br />
+			<br />
+			{/* <Design/> */}
+			<h2 className={s.welcome}>
+				Wecome to Login Page, enter your details to be logged into your
+				account!!
+			</h2>
+			<br />
+			<div className={s.content}>
+				<form onSubmit={submit}>
+					<br />
+					<div className={s.formGroup}>
+						<label htmlFor="email">Username: </label>
+						<input
+							type="text"
+							id="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className={s.formGroup}>
+						<label htmlFor="password">Password: </label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<button type="submit">Submit</button>
 
-    return (
-        <main className={s.login}><br /><br />
-            <Design/>
-            <h2 className={s.welcome}>Wecome to Login Page, enter your details to be logged into your account!!</h2><br />
-            <div className={s.content}> 
-                <form onSubmit={submit}><br />
-                    <div className={s.formGroup}>
-                        <label htmlFor="email">Username: </label>
-                        <input
-                            type="text"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className={s.formGroup}>
-                        <label htmlFor="password">Password: </label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit">Submit</button>
-                    
-                    <br />
-                    
+					<br />
 
-                    { loading ? <p>loading</p> : null }
-                    { error ? <p>wrong credentials</p> : null }
-
-                </form>
-            </div>
-            <p>P.S. You will be redirected on successful login</p>
+					{loading ? <p>loading</p> : null}
+					{error ? <p>wrong credentials</p> : null}
+				</form>
+			</div>
+			<p>P.S. You will be redirected on successful login</p>
 		</main>
-    );
+	);
 }
--- frontend/src/app/questions/_components/QuestionModal.tsx
+++ frontend/src/app/questions/_components/QuestionModal.tsx
@@ -1,116 +1,128 @@
-import Question from "@/models/Question"
-import s from "./QuestionModal.module.css"
-import { useState } from "react"
-import ENDPOINT from "@/helpers/endpoint"
-import { useCookies } from "react-cookie"
-import { ClipLoader } from "react-spinners"
+import Question from "@/models/Question";
+import s from "./QuestionModal.module.css";
+import { useState } from "react";
+import ENDPOINT from "@/helpers/endpoint";
+import { useCookies } from "react-cookie";
+import { ClipLoader } from "react-spinners";
 
-type Props = { question: Question, close: () => void}
+type Props = { question: Question; close: () => void };
 
-export default function QuestionModal ({ question, close } : Props) {
-    let [ answer, setAnswer ] = useState("")
-    let [ errorText, setErrorText ] = useState("")
-    let [ successText, setSuccessText ] = useState(question.is_answered ? "Already answered!" : "")
-    let [ loading, setLoading ] = useState(false)
+export default function QuestionModal({ question, close }: Props) {
+	let [answer, setAnswer] = useState("");
+	let [errorText, setErrorText] = useState("");
+	let [successText, setSuccessText] = useState(
+		question.is_answered ? "Already answered!" : ""
+	);
+	let [loading, setLoading] = useState(false);
 
-    let [ closing, setClosing ] = useState(false)
+	let [closing, setClosing] = useState(false);
 
-    const [cookies] = useCookies(['access_token']);
-    const access_token = cookies.access_token;
+	const [cookies] = useCookies(["access_token"]);
+	const access_token = cookies.access_token;
 
-    let answered = successText !== ""
-    
-    async function submit () {
-        if (answered) return
-        
-        setLoading(true)
-        setErrorText(" ")
+	let answered = successText !== "";
 
-        try {
-            let res = await fetch(ENDPOINT + "/response/", {
-                method: "POST",
-                body: JSON.stringify({
-                    flag: answer,
-                    id: question.id
-                }),
-                headers: {
-                    'Authorization': `Bearer ${access_token}`,
-                    'Content-Type': "application/json"
-                }
-            })
+	async function submit() {
+		if (answered) return;
 
-            setLoading(false)
-            
-            if (res.status == 400) {
-                setErrorText("wrong answer :(")
-            } else if (res.status == 404) {
-                setErrorText("you have already answered this question")
-            } else if (!res.ok) {
-                throw new Error()
-            } else {
-                setErrorText("")
-                setAnswer("")
-                setSuccessText("Correct!")
-            }
-        } catch (err) {
-            setLoading(false)
-            setErrorText("some error occured")
-        }
-    }
+		setLoading(true);
+		setErrorText(" ");
 
-    function handleClose () {
-        setClosing(true)
-        setTimeout(close, 300)
-    }
+		try {
+			let res = await fetch(ENDPOINT + "/response/", {
+				method: "POST",
+				body: JSON.stringify({
+					flag: answer,
+					id: question.id,
+				}),
+				headers: {
+					Authorization: `Bearer ${access_token}`,
+					"Content-Type": "application/json",
+				},
+			});
 
-    return (
-        <div className={`${s.container} ${closing ? s.closing : ""}`} onClick={handleClose}>
-            <div className={s.modal} onClick={e => e.stopPropagation()}>
-                <div className={s.content}>
-                    <h2>{question.title}</h2>
-                    {
-                        question.text.split("\n").map((text, i) => {
-                            return <p key={i}>{text}</p>
-                        })
-                    }
-                    {
-                        question.link !== "" ?
-                            <a target="_blank" href={question.link}>Link to Resources</a>
-                        : null
-                    }<br /><br />
-                </div>
-                <div className={s.bottom}>
-                    <div className={s.response}>
-                        <div className={s.textbox}>
-                            <input type="text" value={answer} onChange={e => setAnswer(e.target.value)} disabled={answered} />
-                        </div>
-                        <div className={`${s.button} ${answered ? s.disabled : ""}`} onClick={submit}>Answer</div>
-                        { 
-                            loading ? 
-                                <ClipLoader color = "#3489db" /> 
-                            : null 
-                        }
-                        {
-                            errorText !== "" ?
-                                <p id={s.error}>{errorText}</p> 
-                            : null
-                        }
-                        {
-                            answered ?
-                                <p id={s.success}>{successText}</p>
-                            : null
-                        }
-                    </div>
-                    <div className={s.points}>
-                        {
-                            answered ? "✔" : ""
-                        }
-                        &nbsp;&nbsp;&nbsp;
-                        { question.points } Points
-                    </div>
-                </div>
-                <span className="material-symbols-outlined" id = {s.close} onClick={handleClose}>close</span>
-            </div>
-        </div>
-    )
+			setLoading(false);
+
+			if (res.status == 400) {
+				setErrorText("wrong answer :(");
+			} else if (res.status == 404) {
+				setErrorText("you have already answered this question");
+			} else if (!res.ok) {
+				throw new Error();
+			} else {
+				setErrorText("");
+				setAnswer("");
+				setSuccessText("Correct!");
+			}
+		} catch (err) {
+			setLoading(false);
+			setErrorText("some error occured");
+		}
+	}
+
+	function handleClose() {
+		setClosing(true);
+		setTimeout(close, 300);
+	}
+
+	return (
+		<div
+			className={`${s.container} ${closing ? s.closing : ""}`}
+			onClick={handleClose}
+		>
+			<div className={s.modal} onClick={(e) => e.stopPropagation()}>
+				<div className={s.dot1}></div>
+				<div className={s.dot2}></div>
+				<div className={s.dot3}></div>
+				<div className={s.dot4}></div>
+				<div className={s.content}>
+					<h2>{question.title}</h2>
+					{question.text.split("\n").map((text, i) => {
+						return <p key={i}>{text}</p>;
+					})}
+					{question.link !== "" ? (
+						<a target="_blank" href={question.link}>
+							Link to Resources
+						</a>
+					) : null}
+					<br />
+					<br />
+				</div>
+				<div className={s.bottom}>
+					<div className={s.response}>
+						<div className={s.textbox}>
+							<input
+								type="text"
+								value={answer}
+								placeholder="Enter the Flag"
+								onChange={(e) => setAnswer(e.target.value)}
+								disabled={answered}
+							/>
+						</div>
+						<div
+							className={`${s.button} ${answered ? s.disabled : ""}`}
+							onClick={submit}
+						>
+							Answer
+						</div>
+						{loading ? <ClipLoader color="#3489db" /> : null}
+						{errorText !== "" ? <p id={s.error}>{errorText}</p> : null}
+						{answered ? <p id={s.success}>{successText}</p> : null}
+					</div>
+					<div className={s.points}>
+						{answered ? "✔" : ""}
+						&nbsp;&nbsp;&nbsp;
+						{question.points} Points
+					</div>
+				</div>
+				<span
+					className="material-symbols-outlined"
+					id={s.close}
+					onClick={handleClose}
+				>
+					close
+				</span>
+			</div>
+		</div>
+	);
 }
